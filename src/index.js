4efddc129d0be874f627662c240da6a1a82768b6
Mario Bermudez Jr <mario.bermudez.jr@outlook.com>
2019-05-31 18:35:55 -0700

    [refactor] Add redux and implement one action with reducer

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+import { searchRobots } from './reducers';
+import './index.css';
+import App from './containers/App';
+import registerServiceWorker from './registerServiceWorker';
+import 'tachyons';
+
+const store = createStore(searchRobots);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />,
+  </Provider>,
+  document.getElementById('root')
+);
+registerServiceWorker();
