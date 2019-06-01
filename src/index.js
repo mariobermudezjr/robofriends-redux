67021674fe3937feb88817126d3f0ff46dd1eadb
Mario Bermudez Jr <mario.bermudez.jr@outlook.com>
2019-06-01 13:18:24 -0700

    [refactor] Add middleware for combining reducers and Add redux-thunk with a request action.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk';
 import { createLogger } from 'redux-logger';
 
-import { searchRobots } from './reducers';
+import { searchRobots, requestRobots } from './reducers';
 import './index.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
@@ -13,8 +14,11 @@ import 'tachyons';
 // Logs out the action and the data associated with that action
 const logger = createLogger();
 
+// Combines all the reduces into one root object reducer
+const rootReducer = combineReducers({ searchRobots, requestRobots });
+
 // Creates a store that will save our data on the client side
-const store = createStore(searchRobots, applyMiddleware(logger));
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger));
 
 ReactDOM.render(
   <Provider store={store}>
